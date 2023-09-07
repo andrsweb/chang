e47078ba0bc851da8add0490870905327cd5e6dd
andrsweb <proflinervk@gmail.com>
2023-09-07 16:46:36 +0700

    add loader

--- src/js/common/common.js
+++ src/js/common/common.js
@@ -1,12 +1,20 @@
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
+import { setTargetElement, getTargetElement } from './global'
+
+
 document.addEventListener('DOMContentLoaded', () => {
 	'use strict'
 })
 
 window.onload = function () {
 	const loader = document.querySelector('.loader')
-	loader.classList.add('loaded_hiding');
+	setTargetElement(document.querySelector('#load-lock'))
+
+	loader.classList.add('closed');
+	disableBodyScroll(getTargetElement(), { reserveScrollBarGap: true })
 	window.setTimeout(function () {
-		loader.classList.add('loaded');
-		loader.classList.remove('loaded_hiding');
+		loader.classList.add('opened');
+		loader.classList.remove('closed');
+		enableBodyScroll(getTargetElement())
 	}, 500);
 }
