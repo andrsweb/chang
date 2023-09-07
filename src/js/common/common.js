c5df0c71cceef6cc741bc05544bcda8e1240779f
andrsweb <proflinervk@gmail.com>
2023-09-07 16:26:59 +0700

    added loader

--- src/js/common/common.js
+++ src/js/common/common.js
@@ -1,3 +1,12 @@
-document.addEventListener( 'DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 	'use strict'
-} )
+})
+
+window.onload = function () {
+	const loader = document.querySelector('.loader')
+	loader.classList.add('loaded_hiding');
+	window.setTimeout(function () {
+		loader.classList.add('loaded');
+		loader.classList.remove('loaded_hiding');
+	}, 500);
+}
